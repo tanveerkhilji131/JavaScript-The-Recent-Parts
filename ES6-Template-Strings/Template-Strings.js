1b6eaa36911991ea5fb57a698816b84f67c42720
tanveer_khilji <tanveerkhilji972@gmail.com>
2025-05-30 19:23:01 +0530

    Add notes on ES6 Template Strings from Kyle Simpson lecture

---
+++ ES6-Template-Strings/Template-Strings.js
@@ -0,0 +1,10 @@
+let name = "tanveer";
+let age = 17;
+
+// ES5  Interpolation
+console.log("my name is "+ name + " and my age is " + age);
+
+// ES6 using template string
+console.log(`my name is ${name} and my age is ${age}`);
+
+// String interpolation using ES5 and ES6 template literals
