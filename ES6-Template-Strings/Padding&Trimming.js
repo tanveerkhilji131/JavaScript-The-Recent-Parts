9fecc772ba5fb9bc4e66ca68318d75134eaaf871
tanveer_khilji <tanveerkhilji972@gmail.com>
2025-05-31 17:08:53 +0530

    Added examples for padStart, padEnd, and trim methods in JS

---
+++ ES6-Template-Strings/Padding&Trimming.js
@@ -0,0 +1,28 @@
+var myname = "tanveer";
+// string staring and ending add padding(space) using padStart() and padEnd() method
+
+// padStart add left side padding 
+
+myname.padStart(7)    //"tanveer"
+
+myname.padStart(10)    //"   tanveer"
+
+myname.padStart(10, "~")   //"~~~tanveer"
+
+myname.padStart(10, "12345") //"123tanveer"
+
+myname.padStart(10, "|Y") //"|Y|tanveer"
+
+
+// padEnd() same padStart and padend add a right side padding
+
+myname.padEnd(7)    //"tanveer"
+
+myname.padEnd(10)    //"tanveer   "
+
+myname.padEnd(10, "~")   //"tanveer~~~"
+
+myname.padEnd(10, "12345") //"tanveer123"
+
+myname.padEnd(10, "|Y")   //"tanveer|Y|"
+
